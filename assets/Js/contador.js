491ad3db49fca3c1f21d6ac5aa32fadf0e2ecc07
Ligiane Vieira <87396681+LigianeVieira@users.noreply.github.com>
2022-01-02 23:11:04 -0300

    Add files via upload

---
+++ assets/Js/contador.js
@@ -0,0 +1,29 @@
+var currentNumberWrapper = document.getElementById("currentNumber");
+var currentNumber = 0;
+
+document.getElementById("adicionar").addEventListener("click", increment);
+document.getElementById("subtrair").addEventListener("click", decrement);
+
+function increment() {
+   if (currentNumber < 0 || currentNumber < 10)
+      currentNumber = currentNumber + 1;
+   currentNumberWrapper.innerHTML = currentNumber;
+
+   if (currentNumber >= 0) {
+      currentNumberWrapper.innerHTML = currentNumber;
+      currentNumberWrapper.style.color = 'blue';
+
+   }
+}
+
+function decrement() {
+   if (currentNumber <= 10 || currentNumber === 0) {
+      currentNumber = currentNumber - 1;
+      currentNumberWrapper.innerHTML = currentNumber;
+   }
+   if (currentNumber < 0) {
+      currentNumberWrapper.innerHTML = currentNumber;
+      currentNumberWrapper.style.color = 'red';
+
+   }
+}
